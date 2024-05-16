56bb89bbf585af15ddce1066631bba38a08a8968
laisuk <laisuk@yahoo.com>
2024-05-16 14:19:06 +0800

    Update Cython Linux

--- opencc_rs_cython/src/setup.py
+++ opencc_rs_cython/src/setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Build import cythonize
+import platform
 
 # Load opencc_fmmseg_capi.dll.lib
+system = platform.system()
+extra_link_args = []
+if system == "Linux":
+    extra_link_args = ["-Wl,-rpath,$ORIGIN"]
+elif system == "Darwin":
+    extra_link_args = ["-Wl,-rpath,@loader_path"]
+
 extensions = [
-    Extension("opencc_fmmseg_capi_wrapper",
-              ["opencc_fmmseg_capi_wrapper.pyx"],
-              libraries=["opencc_fmmseg_capi.dll"],
+    Extension("opencc_fmmseg_capi_wrapper", ["opencc_fmmseg_capi_wrapper.pyx"],
+              libraries=["opencc_fmmseg_capi"],
               include_dirs=["."],
-              library_dirs=["."]
+              library_dirs=["."],
+              extra_link_args=extra_link_args
               ),
 ]
 
