a0cc97a9760f75ef76df8736e5d16aa3d1ea756a
laisuk <laisuk@yahoo.com>
2025-07-18 17:46:55 +0800

    Add opencc_pyo3 module

--- mainwindow.py
+++ mainwindow.py
@@ -8,10 +8,10 @@ from PySide6.QtGui import QGuiApplication
 from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
 
 # from opencc_cython import OpenCC
-from opencc_purepy import OpenCC
+# from opencc_purepy import OpenCC
 # from opencc_jieba_pyo3 import OpenCC
 # from opencc_py import OpenCC
-# from opencc_pyo3 import OpenCC
+from opencc_pyo3 import OpenCC
 # from opencc_rs import OpenCC
 # Important:
 # You need to run the following command to generate the ui_form.py file
